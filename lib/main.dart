2a19c38ad866e1feb5b26e19174018839894a167
susumOyaji <sumitomo0210@gmail.com>
2019-11-16 16:18:28 +0900

    up

--- lib/main.dart
+++ lib/main.dart
@@ -3,53 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-
-
-GridView gridView1() => new GridView.builder(
-  //itemCount: widgets.length,//<-- setState()
-  //itemCount: 3,
-  gridDelegate:new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
-  itemBuilder: (BuildContext context, int index) {
-    return new GestureDetector(
-      child: new Card(
-        color: Colors.grey[850],
-        elevation: 5.0,
-        child: new Container(
-          //color: Colors.black38,
-          //padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-          //alignment: Alignment.center,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text("({widgets[index].code}) "+"{widgets[index].name}",
-                  style: TextStyle(fontSize: 9.0, color: Colors.white),),
-              new Text("現在値 {separate(widgets[index].realValue.toString())}",
-                  style: TextStyle(fontSize: 10.0, color: Colors.white),),
-              new Text("利益　{separate(widgets[index].gain.toString())}",
-                  style: TextStyle(fontSize: 10.0, fontFamily: 'Roboto',color: Colors.yellow),),
-              Chip(
-                avatar: CircleAvatar(
-                  backgroundColor: Colors.grey.shade800,
-                  child: Text('AB'),
-                ),
-                label: Text('Aaron Burr'),
-              )
-    
-            ],
-              
-          ),
-                //alignment: Alignment.center,
-                //child: new Text("${widgets[index].code}",style: TextStyle(fontSize: 10.0, color: Colors.white),),//new Text('Item $index'),
-        ),
-                //alignment: Alignment.center,
-                //child: new Text('Item $index'),
-      ),
-    );
-  });
-
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 
@@ -64,39 +18,147 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+int _counter = 0;
+String load;
 Future post;
  var _chipList = List<Chip>();
  var _keyNumber = 0;
- String price;
+ String price ="";
 
   @override
   void initState() {
     super.initState();
-    post = fetch();
-    _addChip("price");
+    //post = fetch(price);
+    _saveString("1000","SONY");//save
+    _loadString("1000");//load
+    
+    
+
+    _addChip("Nikkei 23300");
+    loadData();
+     _addChip("Newyork 27000");
+    _addChip("Taiyo");
+    //_addChip(load);
+    _addChip("3");
+    _addChip("4");
         //print("out");
   }
+
+  //Incrementing counter after click
+  _incrementCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0) + 1;
+      prefs.setInt('counter', _counter);
+    });
+  }
+
+  //Incrementing counter after click
+  _decrementCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0) - 1;
+      prefs.setInt('counter', _counter);
+    });
+  }
+
+  //Loading counter value on start
+  void _loadString( String keyword) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      load = prefs.getString(keyword) ?? "null";//load
+      print(load);
+    });
+  }
+
+  //Incrementing counter after click
+  void _saveString(String keyword,String keydata) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+        prefs.setString(keyword, keydata);//save
+        _incrementCounter();//save to countUp
+    });
+  }
+
+  _deleteCounter(String keyword) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove(keyword);
+    _decrementCounter();//save to countDown
+  }
+
+
+
 
 
 void _addChip(String text) {
     var chipKey = Key('chip_key_$_keyNumber');
     _keyNumber++;
-
+    
     _chipList.add(
       Chip(
         key: chipKey,
-        label: Text(text),
+        backgroundColor:  Colors.grey,
+        elevation: 4,
+        shadowColor: Colors.orange,
+        padding: EdgeInsets.all(4),
+        avatar: CircleAvatar(
+          backgroundColor: Colors.green,//.grey.shade800,
+          child: Text(_keyNumber.toString()),
+        ),
+        label: Text(text,style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
         onDeleted: () => _deleteChip(chipKey),
       ),
     );
+
   }
 
+
+ 
+
+  
   void _deleteChip(Key chipKey) {
     setState(() => _chipList.removeWhere((Widget w) => w.key == chipKey));
   }
 
+loadData() async {
+    String responce ="6758,200,1665\n9837,200,712\n6976,200,1746\n6753,0,0\n";
+    List prices = json.decode(responce);
+    _incrementCounter();
 
-Future fetch() async {
+    for(String price in prices) {
+      
+      //fetch(price);
+      _addChip("SONY"+" 1,234");
+    }
+}
+
+static List<Price> parse(var responce)
+        {
+            List<Price> prices = new List<Price>(); //ｺﾝｽﾄﾗｸﾀ
+            var list = responce.split("\n"); 
+              
+              for( String row in list ) {
+                if (row.isNotEmpty){
+                    //continue;
+                  var lista = row.split(",");  
+                  //Price p = new Price(lista[0],lista[1],lista[2],lista[3],lista[4],lista[5],list[6]);
+                  Price p = new Price();
+                  p.code = lista[0];//企業コード
+                  p.stocks = (lista[1]);//保有数
+                  p.itemprice = (lista[2]);//購入単価
+                  prices.add(p);
+
+                  print('Finance=${list.indexOf(row)}: $row');
+                }
+              }                   
+           return prices;
+        }
+
+
+
+
+
+Future fetch(price) async {
   final  response =
       await http.get("https://stocks.finance.yahoo.co.jp/stocks/detail/?code=6976.T");//^DJI
       final String json = response.body;
@@ -122,7 +184,10 @@ Future fetch() async {
       //}
 
       RegExp regExp = RegExp(r'[0-9]{1,},[0-9]{1,}');//new RegExp(r"/[0-9]+/");
-      price = regExp.stringMatch(json).toString();
+      setState(() {
+        price = regExp.stringMatch(json).toString();
+      });
+      
       print("StockPrice : "+price);
       
       var intprice;
@@ -186,8 +251,12 @@ Future fetch() async {
     throw Exception('Failed to load post');
   }
   */
-//////////////////////////////
+
 }
+
+
+
+
 
 
 
@@ -198,59 +267,98 @@ Future fetch() async {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+       return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
           title: Text('Fetch Data Example'),
         ),
         body:GridView.count(
-        crossAxisCount: 2,
+        crossAxisCount: 1,
         children: <Widget>[
           Container(
-            color: Colors.yellowAccent,
+            color: Colors.black,
             height: double.infinity, // tambahkan property berikut
             child: Center(
-              child: Text("1", style: TextStyle(fontSize: 24.0),),
-            ),
-          ),
-          Container(
-            color: Colors.blueAccent,
-            height: double.infinity, // tambahkan property berikut
-            child: Center(
-              child: Text("2", style: TextStyle(fontSize: 24.0),),
-            ),
-          ),
-          Container(
-            color: Colors.brown,
-            height: double.infinity, // tambahkan property berikut
-            child: Center(
-              child: Text("3", style: TextStyle(fontSize: 24.0),),
-            ),
-          ),
-          Container(
-            color: Colors.orange,
-            height: double.infinity, // tambahkan property berikut
-            child: Center(
-              child:Row(children: <Widget>[
+              child:Column(children: <Widget>[
                 Expanded(
                   child: Wrap(
                     alignment: WrapAlignment.start,
-                    spacing: 8.0,
-                    runSpacing: 0.0,
+                    spacing: 10.0,// gap between adjacent chips
+                    runSpacing: 0.0,// gap between lines
+                    direction: Axis.horizontal,
+                    children: _chipList,
+                  ),
+                ), 
+               ],
+               )
+            ),
+          
+          ),    
+          Container(
+            color: Colors.green[100],
+            height: double.infinity, // tambahkan property berikut
+            child: Center(
+              child:Column(children: <Widget>[
+                Expanded(
+                  child: Wrap(
+                    alignment: WrapAlignment.start,
+                    spacing: 10.0,// gap between adjacent chips
+                    runSpacing: 0.0,// gap between lines
                     direction: Axis.horizontal,
                     children: _chipList,
                   ),
                 ),
-                //_addChip("test"),// Text("4", style: TextStyle(fontSize: 24.0),),
+               
+                
+              Wrap(
+                spacing: 8.0, // gap between adjacent chips
+                runSpacing: 4.0, // gap between lines
+                children: <Widget>[
+                  Chip(
+                  avatar: CircleAvatar(backgroundColor: Colors.blue.shade900, child: Text('AH')),
+                  label: Text(load),
+                ),
+   
+  ],
+)
+                // Text("4", style: TextStyle(fontSize: 24.0),),
             ],)
             ),
           ),
-          
+         
         ],
       ),
-      
+     
     ),
     );   
     
+ 
   }
 }
+
+
+class Price
+    {
+        String code ;//会社名コード
+        String name;//会社名*
+        var stocks;//保有数*
+        dynamic itemprice;//購入価格*
+        double realprice;//現在値**
+        String prevday;//前日比±**
+        String percent; //前日比％**
+        String polar; //上げ下げ(+ or -)
+        dynamic ayAssetprice;//保有数* 購入価格 = 投資総額
+        dynamic realValue;//利益総額
+       
+        dynamic investmen;//投資額
+        dynamic investmens;//投資総額
+        dynamic uptoAsset;//個別利益
+        dynamic totalAsset;//現在評価額合計
+       
+        double gain;//損益
+        
+        int idindex;
+
+       
+         //Price(this.code, this.stocks, this.itemprice,this.name,this.realValue,this.prev_day,this.percent );
+    }
