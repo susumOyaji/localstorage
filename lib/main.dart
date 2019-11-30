7837230b917f19629518fb8bd788f65042f949c6
susumOyaji <sumitomo0210@gmail.com>
2019-11-30 17:13:45 +0900

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'key_finder_interface.dart';
-//import 'package:localstorage/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-//void main() => runApp(MyApp());
+    void main() => runApp(MyApp());
 
-void main() {
-  debugPaintSizeEnabled = false;
-  runApp(
-    MaterialApp(
-      home: MyApp(),
-    ),
-  );
-}
+    class MyApp extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return MaterialApp(
+          theme: ThemeData(primarySwatch: Colors.blue),
+          home: MyHomePage(),
+        );
+      }
+    }
 
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    KeyFinder keyFinder = KeyFinder();
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SafeArea(
-        child: KeyValueWidget(
-          keyFinder: keyFinder,
-        ),
-      ),
-    );
-  }
-}
-
-class KeyValueWidget extends StatefulWidget {
-  final KeyFinder keyFinder;
-
-  KeyValueWidget({this.keyFinder});
-  @override
-  _KeyValueWidgetState createState() => _KeyValueWidgetState();
-}
-
-class _KeyValueWidgetState extends State<KeyValueWidget> {
-  String key = "MyKey";
-  TextEditingController _keyTextController = TextEditingController();
-  TextEditingController _valueTextController = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        width: 200.0,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Text(
-                '$key / ${widget.keyFinder.getKeyValue(key)}',
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Expanded(
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Key",
-                  border: OutlineInputBorder(),
+    class MyHomePage extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Saving data'),
+          ),
+          body: Row(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  child: Text('Read'),
+                  onPressed: () {
+                    _read();
+                  },
                 ),
-                controller: _keyTextController,
               ),
-            ),
-            Expanded(
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Value",
-                  border: OutlineInputBorder(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  child: Text('Save'),
+                  onPressed: () {
+                    _save();
+                  },
                 ),
-                controller: _valueTextController,
               ),
-            ),
-            RaisedButton(
-              child: Text('Save new Key/Value Pair'),
-              onPressed: () {
-                widget.keyFinder.setKeyValue(
-                  _keyTextController.text,
-                  _valueTextController.text,
-                );
-                setState(() {
-                  key = _keyTextController.text;
-                });
-              },
-            ),
-            RaisedButton(
-              child: Text('Load'),
-              onPressed: () {
-                widget.keyFinder.getKeyValue(key);
-                setState(() {
-                  widget.keyFinder.getKeyValue(key);
-                });
-              },
-            )
+            ],
+          ),
+        );
+      }
 
-          ],
-        ),
-      ),
-    );
-  }
-}
+      // Replace these two methods in the examples that follow
+
+      _read() async {
+        final prefs = await SharedPreferences.getInstance();
+        final key = 'my_int_key';
+        final value = prefs.getInt(key) ?? 0;
+        print('read: $value');
+      }
+
+      _save() async {
+        final prefs = await SharedPreferences.getInstance();
+        final key = 'my_int_key';
+        final value = 42;
+        prefs.setInt(key, value);
+        print('saved $value');
+      }
+    }
---
+++ lib/mainorg.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'key_finder_interface.dart';
+//import 'package:localstorage/main.dart';
+
+//void main() => runApp(MyApp());
+
+void main() {
+  debugPaintSizeEnabled = false;
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    KeyFinder keyFinder = KeyFinder();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SafeArea(
+        child: KeyValueWidget(
+          keyFinder: keyFinder,
+        ),
+      ),
+    );
+  }
+}
+
+class KeyValueWidget extends StatefulWidget {
+  final KeyFinder keyFinder;
+
+  KeyValueWidget({this.keyFinder});
+  @override
+  _KeyValueWidgetState createState() => _KeyValueWidgetState();
+}
+
+class _KeyValueWidgetState extends State<KeyValueWidget> {
+  String key = "MyKey";
+  TextEditingController _keyTextController = TextEditingController();
+  TextEditingController _valueTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        width: 200.0,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                '$key / ${widget.keyFinder.getKeyValue(key)}',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Expanded(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Key",
+                  border: OutlineInputBorder(),
+                ),
+                controller: _keyTextController,
+              ),
+            ),
+            Expanded(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Value",
+                  border: OutlineInputBorder(),
+                ),
+                controller: _valueTextController,
+              ),
+            ),
+            RaisedButton(
+              child: Text('Save new Key/Value Pair'),
+              onPressed: () {
+                widget.keyFinder.setKeyValue(
+                  _keyTextController.text,
+                  _valueTextController.text,
+                );
+                setState(() {
+                  key = _keyTextController.text;
+                });
+              },
+            ),
+            RaisedButton(
+              child: Text('Load'),
+              onPressed: () {
+                widget.keyFinder.getKeyValue(key);
+                setState(() {
+                  widget.keyFinder.getKeyValue(key);
+                });
+              },
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
